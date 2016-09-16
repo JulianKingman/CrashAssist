122e24e77d1f253dd97b1e2ef42561c1e0c56645
Julian Kingman <julian@designman.io>
2016-09-16 17:18:02 -0400

    added basemodel and allow to Users

--- api/collections/Users.js
+++ api/collections/Users.js
@@ -3,6 +3,20 @@
  */
 Users = Meteor.users();
 
+//collection hooks
+
+// Users.before.insert((userId, doc) => {});
+// Users.after.insert((userId, doc) => {});
+// Users.before.update((userId, doc, fieldNames, modifier, options) => { });
+// Users.after.update((userId, doc, fieldNames, modifier, options) => {});
+// Users.before.remove((userId, doc) => {});
+// Users.after.remove((userId, doc) => {});
+// Users.before.upsert((userId, selector, modifier, options) => {});
+// Users.before.find((userId, selector, options) => {});
+// Users.after.find((userId, selector, options, cursor) => {});
+// Users.before.findOne((userId, selector, options) => {});
+// Users.after.findOne((userId, selector, options, doc) => {});
+
 class User extends BaseModel {
     constructor(document) {
         super(document);  //Must call super passing in the document.
@@ -20,5 +34,14 @@ class User extends BaseModel {
         return UserInputs.find({user_id: this._id});
     }
 }
+
+Users.allow({
+    insert(userId, userIncident){
+    },
+    update(userId, userIncident){
+    },
+    remove(userId, userIncident) {
+    }
+});
 
 export {Users, User};
