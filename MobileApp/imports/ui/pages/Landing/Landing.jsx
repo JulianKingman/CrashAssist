01c3a306397e38d291ec30d066746fc3e5cee85f
copleykj <copleykj@gmail.com>
2016-09-18 00:30:10 -0400

    landing prototype

--- MobileApp/imports/ui/pages/Landing/Landing.jsx
+++ MobileApp/imports/ui/pages/Landing/Landing.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Page, Button } from 'react-onsenui';
+
+import './Landing.scss';
 
 export default class Landing extends Component {
     render(){
         return (
-            <h1>Welcome to Crash Assist!</h1>
+            <Page key="landing">
+                <div id="landing">
+                    <h1>Remain Calm, <span>We'll help you through this!</span></h1>
+                    <img src="images/crash.svg" />
+                    <Button modifier="large outline">New Incident</Button>
+                    <p>In an accident? Start here.</p>
+                    <Button modifier="large outline">Past Incidents</Button>
+                </div>
+            </Page>
         );
     }
 }
