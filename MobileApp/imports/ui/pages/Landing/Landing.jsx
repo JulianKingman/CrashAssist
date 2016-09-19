1fbbb9f30c7b65efcb589f115ff296794e1b9862
copleykj <copleykj@gmail.com>
2016-09-18 22:49:19 -0400

    adjust styling for android

--- MobileApp/imports/ui/pages/Landing/Landing.jsx
+++ MobileApp/imports/ui/pages/Landing/Landing.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { Page, Button } from 'react-onsenui';
+import { Page, Button, Ripple } from 'react-onsenui';
+import onsen from 'onsenui';
 
 import './Landing.scss';
 
 export default class Landing extends Component {
     render(){
+
+        let modifier = "large";
+        let ripple = false;
+
+
+        if(!onsen.platform.isAndroid()){
+            modifier +=  " outline";
+        } else {
+            ripple = <Ripple />;
+        }
+
         return (
             <Page key="landing">
                 <div id="landing">
                     <h1>Remain Calm, <span>We'll help you through this!</span></h1>
                     <img src="images/crash.svg" />
-                    <Button modifier="large outline">New Incident</Button>
+                    <Button modifier={modifier} >{ripple}New Incident</Button>
                     <p>In an accident? Start here.</p>
-                    <Button modifier="large outline">Past Incidents</Button>
+                    <Button modifier='large outline' >{ripple}Past Incidents</Button>
                 </div>
             </Page>
         );
