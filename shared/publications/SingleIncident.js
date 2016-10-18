f44f828dbdbb8fc9761c2500564ce21b6bba093a
copleykj <copleykj@gmail.com>
2016-10-18 00:26:57 -0400

    fix imports

--- shared/publications/SingleIncident.js
+++ shared/publications/SingleIncident.js
@@ -1,4 +1,4 @@
-import { Incidents } from '/imports/shared/collections/Incidents.js';
+import { Incidents } from '/imports/api/collections/Incidents.js';
 
 Meteor.publish('SingleIncident', function(_id) {
     return Incidents.find({userid:this.userId, _id});
