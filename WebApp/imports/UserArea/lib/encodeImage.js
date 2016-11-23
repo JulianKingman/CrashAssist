c0dd3848b6cb3cef0b6c327e0935f63128106dfc
Julian Kingman <julian@designman.io>
2016-11-23 11:56:31 -0500

    added pdf export

---
+++ WebApp/imports/UserArea/lib/encodeImage.js
@@ -0,0 +1,148 @@
+/**
+ * Created by Julian on 11/20/16.
+ */
+export function getBinary(file, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(e) {
+        var arr = new Uint8Array(this.response);
+        let raw = '';
+        let i,j,subArray,chunk = 5000;
+        for (i=0,j=arr.length; i<j; i+=chunk) {
+            subArray = arr.subarray(i,i+chunk);
+            raw += String.fromCharCode.apply(null, subArray);
+        }
+        var b64=btoa(raw);
+        var dataURL="data:image/jpeg;base64,"+b64;
+        callback(null, dataURL);
+    };
+    xhr.send();
+}
+
+function arrayBufferDataUri(raw) {
+    var base64 = '';
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    var bytes = new Uint8Array(raw);
+    var byteLength = bytes.byteLength;
+    var byteRemainder = byteLength % 3;
+    var mainLength = byteLength - byteRemainder;
+
+    var a, b, c, d;
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63 // 63       = 2^6 - 1
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 16128) >> 8 // 16128 = (2^6 - 1) << 8
+        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return "data:image/jpeg;base64," + base64
+}
+
+export function base64Encode(str) {
+    var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var out = "", i = 0, len = str.length, c1, c2, c3;
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += CHARS.charAt(c1 >> 2);
+            out += CHARS.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += CHARS.charAt(c1 >> 2);
+            out += CHARS.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+            out += CHARS.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += CHARS.charAt(c1 >> 2);
+        out += CHARS.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += CHARS.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += CHARS.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+
+export function base64ArrayBuffer(arrayBuffer) {
+    var base64    = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var bytes         = new Uint8Array(arrayBuffer)
+    var byteLength    = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength    = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}
--- WebApp/imports/UserArea/ui/pages/Incidents/IncidentSingle.jsx
+++ WebApp/imports/UserArea/ui/pages/Incidents/IncidentSingle.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 import {Row, Grid, Col, Table, Glyphicon, Button, Panel, Image} from 'react-bootstrap';
 import {Meteor} from 'meteor/meteor';
 import {Incidents} from '/imports/shared/collections/Incidents.js';
@@ -6,8 +7,9 @@ import TrackerReact from 'meteor/ultimatejs:tracker-react';
 // import {Cloudinary} from 'meteor/lepozepo:cloudinary';
 import Layout from '../../components/Layout.jsx';
 import pageSchema from '/imports/shared/page-schema.js';
-import _ from 'underscore';
-import {pdfMake} from 'pdfmake';
+import _ from 'lodash';
+// const pdfMake = require('pdfmake-node');
+import {getBinary, base64Encode} from '/imports/UserArea/lib/encodeImage.js'
 if (Meteor.isClient) {
     require('./IncidentSingle.scss');
 }
@@ -20,10 +22,10 @@ export default class IncidentSingle extends TrackerReact(Component) {
                 incident: Meteor.subscribe('SingleIncident', this.props.params.incidentId)
             }
         }
+
     }
 
     Incident() {
-        // console.log(this.props.params.incidentId);
         return Incidents.find({_id: this.props.params.incidentId}).fetch()[0];
     };
 
@@ -37,15 +39,15 @@ export default class IncidentSingle extends TrackerReact(Component) {
     };
 
     renderRow = ()=> {
-        const accordions = _.flatten(_.pluck(pageSchema, 'accordions'), true);
+        const accordions = _.flatten(_.map(pageSchema, 'accordions'), true);
         return accordions.map((accordion, index)=> {
             if (accordion.fields) {
                 {
                     return accordion.fields.map((field, index)=> {
                         return (
-                            <tr key={`fieldset-${index}`}>
+                            <tr key={`fieldset-${index}`} data-title={accordion.title}>
                                 <td>{field.label}</td>
-                                <td>{this.renderFields(field, index)}</td>
+                                <td data-type={field.type}>{this.renderFields(field, index)}</td>
                             </tr>
                         )
                     })
@@ -71,15 +73,25 @@ export default class IncidentSingle extends TrackerReact(Component) {
             value = Array.isArray(value) ? value : typeof value === 'undefined' ? [] : [value];
             // console.log(value, value.length);
             if (value.length) {
-                return value.map((photoId, index)=> {
-                    let pictureOpts = {}; //{hash: {width: 55, height: 55, crop: 'fill'}};
-                    let imageUrl = Meteor.isServer ? Cloudinary.utils.url(photoId, pictureOpts) : Cloudinary._helpers.url(photoId, pictureOpts);
-                    return <img src={imageUrl} style={{marginRight: 15}}
-                                key={`${field.name}-photo-${index}`}
-                                onClick={()=> {
-                                    this.enlargeImage(index, value)
-                                }} className="incident-photo"/>
-                })
+                return (
+                    <Row>
+                        {
+                            value.map((photoId, index)=> {
+                                let pictureOpts = {}; //{hash: {width: 55, height: 55, crop: 'fill'}};
+                                let imageUrl = Meteor.isServer ? Cloudinary.utils.url(photoId, pictureOpts) : Cloudinary._helpers.url(photoId, pictureOpts);
+                                return (
+                                    <Col xs={6} sm={4} md={3}>
+                                        <img src={imageUrl} style={{marginRight: 15}}
+                                             key={`${field.name}-photo-${index}`}
+                                             onClick={()=> {
+                                                 this.enlargeImage(index, value)
+                                             }} className="incident-photo"/>
+                                    </Col>
+                                )
+                            })
+                        }
+                    </Row>
+                )
             }
         } else if (field.type === "array") {
             let arrayField = this.Incident()[field.name];
@@ -87,14 +99,22 @@ export default class IncidentSingle extends TrackerReact(Component) {
                 //getting the array of field value groups, i.e. fieldGroup.$
                 return arrayField.map((fieldset, arrayIndex)=> {
                     //getting the fields, e.g. fieldGroup.$.field1, fieldGroup.$.field2
-                    return Object.keys(fieldset).map((key, fieldIndex)=> {
-                        return (
-                            <span key={`item-${index}-${arrayIndex}-${fieldIndex}`} className="witness-row">
-                                <strong>{key}: </strong>{fieldset[key]}
-                            </span>
-                        )
-                    })
-                });
+                    return (
+                        <p key={`item-${index}-${arrayIndex}`}>
+                            {
+                                Object.keys(fieldset).map((key, fieldIndex)=> {
+                                    return (
+                                        <span key={`item-${index}-${arrayIndex}-${fieldIndex}`}
+                                              className="array-field">
+                                                <span className="array-key">{key}: </span><span
+                                            className="array-value">{fieldset[key]}</span>
+                                            </span>
+                                    )
+                                })
+                            }
+                        </p>
+                    )
+                })
             }
         } else {
             if (value) {
@@ -103,12 +123,106 @@ export default class IncidentSingle extends TrackerReact(Component) {
         }
     };
 
+    exportPdf() {
+        // console.log(this.table, this.refs, ReactDOM.findDOMNode('table'));
+        if (Meteor.isClient) {
+            // console.log($('.data-table'));
+            let data = [];
+            //todo: add spinner until this is done
+            let i = 0;
+            const images = $('.data-table img');
+            let convertPhotoSource = (done)=> {
+                // console.log(images[i].src);
+                getBinary(images[i].src, (err, res)=> {
+                    // console.log(`Response: ${res}`);
+                    // console.log(images[i]);
+                    images[i].src = `${res}`;
+                    i++;
+                    if (i < images.length) {
+                        convertPhotoSource(done)
+                    } else {
+                        done()
+                    }
+                });
+            };
+            convertPhotoSource(()=> {
+                //todo: stop spinner
+                console.log("Images swapped!");
+                let tableData = $('.data-table tr');
+                let newHeader = (header, pageBreak)=> {
+                    if (typeof header !== 'undefined') {
+                        let margin = pageBreak === 'before'? [0,0,0,10]: [0,30,0,10];
+                        data.push({text: header, style: 'header', margin, pageBreak});
+                    }
+                };
+                let oldHeader;
+                tableData.map((index, row)=> {
+                    let header = $(row).data('title');
+                    // console.log(header, nextHeader, header === nextHeader);
+                    let content = $($(row).children('td')[1]);
+                    let label = $($(row).children('td')[0]);
+                    // console.log(content, content.data(), content.data('type'));
+                    if (content.data('type') === 'photo') {
+                        let imageArray = $(content).find('img');
+                        if (imageArray.length) {
+                            newHeader(header, 'before');
+                            imageArray.map((index, image)=> {
+                                data.push({image: image.src, margin: [0,10,0,10], fit: [510, 310]})
+                            });
+                            oldHeader = oldHeader !== header? header: oldHeader;
+                        }
+                    } else if (content.data('type') === 'array') {
+                        let array = content.find('p');
+                        if (array.length) {
+                            array.map((index, p)=> {
+                                newHeader(`${header} ${index + 1}`);
+                                let keys = $(p).find('span.array-key').map((i, key)=> {
+                                    return $(key).text();
+                                }).toArray();
+                                let values = $(p).find('span.array-value').map((i, value)=> {
+                                    return $(value).text();
+                                }).toArray();
+                                // console.log(keys, values);
+                                keys.map((k, i)=> {
+                                    data.push({columns: [{text: k, bold: true, width: '33%'}, values[i]]});
+                                });
+                            });
+                            oldHeader = oldHeader !== header? header: oldHeader;
+                        }
+                    } else {
+                        if (label.text().length) {
+                            if(oldHeader !== header) newHeader(header);
+                            data.push({columns: [{text: label.text(), bold: true, width: '33%'}, content.text()]});
+                            oldHeader = oldHeader !== header? header: oldHeader;
+                        }
+                    }
+                });
+                console.log(data);
+                pdfMake.createPdf({
+                    content: data,
+                    styles: {header: {fontSize: 22, bold: true}},
+                    pageSize: 'LETTER',
+                    pageOrientation: 'portrait',
+                    info: {
+                        title: 'Incident Report',
+                        author: 'Driver',
+                        subject: 'Documentation regarding the accident',
+                    }
+                }).open();
+            });
+        }
+    }
+
     render() {
         return (
             <Layout>
+                <canvas id="data-image"/>
                 <Row className="content-wrapper">
                     <Col xs={10} xsOffset={1}>
-                        <Table>
+                        <br/>
+                        <Button onClick={this.exportPdf} className="pull-right">Export PDF</Button>
+                        <br/>
+                        <Table className="data-table">
                             <thead>
                             <tr>
                                 <th>Field</th>
