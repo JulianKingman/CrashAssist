6e7fe9fc0e1633a2fccfeb64fce5836cf10756c2
Julian Kingman <julian@designman.io>
2017-06-20 15:48:41 -0400

    refactored folder structure, breaking change

--- imports/collections/Incidents.js
+++ imports/collections/Incidents.js
@@ -69,71 +69,75 @@ Schemas.Incidents = new SimpleSchema({
 Incidents.attachSchema(Schemas.Incidents);
 
 Schemas.formSchema = new SimpleSchema({
-  safety: {type: Object, optional: true},
-  "safety.carOff": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "safety.safePlace": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "safety.medicalAttention": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "safety.call911": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "safety.waitForPolice": {type: Boolean, optional: true, srf: {type: TextInput}},
-  //dos & donts
-  "dont.admitFault": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "dont.discussAccident": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "dont.diminishInjury": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "dont.acceptPayment": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "do.cooperate": {type: Boolean, optional: true, srf: {type: TextInput}},
-  "do.collectInformation": {type: Boolean, optional: true, srf: {type: TextInput}},
-    //driver info
-    "driverInfo.licensePhoto": {type: Array, optional: true, srf: {type: PhotoInput}},
-    "driverInfo.name": {type: String, optional: true, srf: {type: TextInput}},
-    "driverInfo.address": {type: String, optional: true, srf: {type: TextInput}},
-    "driverInfo.phone": {type: String, optional: true, srf: {type: TelInput}},
-    "driverInfo.email": {type: String, optional: true, srf: {type: EmailInput}},
-    "driverInfo.license": {type: String, optional: true, srf: {type: TextInput}},
-    //[passenger info]
-    "passengerInfo": {type: Array, optional: true, srf: {type: ArrayField}},
-    "passengerInfo.$.name": {type: String, optional: true, srf: {type: TextInput}},
-    "passengerInfo.$.phone": {type: String, srf: {type: TelInput}, optional: true},
-    "passengerInfo.$.email": {type: String, srf: {type: EmailInput}, optional: true},
-    //vehicle info
-    "vehicleInfo.make": {type: String, optional: true, srf: {type: TextInput}},
-    "vehicleInfo.model": {type: String, optional: true, srf: {type: TextInput}},
-    "vehicleInfo.year": {type: Number, optional: true, srf: {type: NumberInput}},
-    "vehicleInfo.plate": {type: String, optional: true, srf: {type: TextInput}},
-    //owner info
-    "ownerInfo.name": {type: String, optional: true, srf: {type: TextInput}},
-    "ownerInfo.address": {type: String, optional: true, srf: {type: TextInput}},
-    "ownerInfo.phone": {type: String, optional: true, srf: {type: TelInput}},
-    "ownerInfo.email": {type: String, optional: true, srf: {type: EmailInput}},
-    "ownerInfo.license": {type: String, optional: true, srf: {type: TextInput}},
-    //insurance info
-    "insuranceInfo.company": {type: String, optional: true, srf: {type: TextInput}},
-    "insuranceInfo.policyNumber": {type: String, optional: true, srf: {type: TextInput}},
-    "insuranceInfo.agent": {type: String, optional: true, srf: {type: TextInput}},
-    //time & location
-    "timeLocation.date": {type: Date, optional: true, srf: {type: DateInput}},
-    "timeLocation.time": {type: String, optional: true, srf: {type: TextInput}},
-    "timeLocation.location": {type: String, optional: true, srf: {type: TextInput}},
-    //Traffic Information
-    "trafficInfo.roadConditions": {type: String, optional: true, srf: {type: TextareaInput}},
-    "trafficInfo.trafficControls": {type: String, optional: true, srf: {type: TextareaInput}},
-    //Witness Information
-    "witnessInfo": {type: Array, optional: true, srf: {type: ArrayField}},
-    "witnessInfo.$": {type: Object, optional: true},
-    "witnessInfo.$.infoPhoto": {type: Array, optional: true, srf: {type: PhotoInput}},
-    "witnessInfo.$.name": {type: String, srf: {type: TextInput}, optional: true},
-    "witnessInfo.$.phone": {type: String, srf: {type: TelInput}, optional: true},
-    "witnessInfo.$.email": {type: String, srf: {type: EmailInput}, optional: true},
-    "witnessInfo.$.testimony": {type: String, srf: {type: TextareaInput}, optional: true},
-    //driver statement
-    "driverStatement": {type: String, srf: {type: TextareaInput}, optional: true},
-    //sketch photo public id
-    "sketch": {type: Array, srf: {type: PhotoInput}, optional: true},
-    //array of photo public ids
-    "photos": {type: Array, srf: {type: PhotoInput}, optional: true},
-    //injuries
-    "injuries": {type: Array, srf: {type: PhotoInput}, optional: true},
-    //Symptoms
-    "symptoms": {type: Array, optional: true, srf: {type: TextInput}}
+  checklist: {type: Object, optional: true},
+  "checklist.carOff": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.safePlace": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.medicalAttention": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.call911": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.waitForPolice": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.admitFault": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.discussAccident": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.diminishInjury": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.acceptPayment": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.cooperate": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.collectInformation": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.operatorsReport": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.notifyInsurance": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.seekMedicalAttention": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.consultAttorney": {type: Boolean, optional: true, srf: {type: TextInput}},
+  "checklist.getWhiplashInfo": {type: Boolean, optional: true, srf: {type: TextInput}},
+  //driver info
+  "driverInfo.licensePhoto": {type: Array, optional: true, srf: {type: PhotoInput}},
+  "driverInfo.name": {type: String, optional: true, srf: {type: TextInput}},
+  "driverInfo.address": {type: String, optional: true, srf: {type: TextInput}},
+  "driverInfo.phone": {type: String, optional: true, srf: {type: TelInput}},
+  "driverInfo.email": {type: String, optional: true, srf: {type: EmailInput}},
+  "driverInfo.license": {type: String, optional: true, srf: {type: TextInput}},
+  //[passenger info]
+  "passengerInfo": {type: Array, optional: true, srf: {type: ArrayField}},
+  "passengerInfo.$.name": {type: String, optional: true, srf: {type: TextInput}},
+  "passengerInfo.$.phone": {type: String, srf: {type: TelInput}, optional: true},
+  "passengerInfo.$.email": {type: String, srf: {type: EmailInput}, optional: true},
+  //vehicle info
+  "vehicleInfo.make": {type: String, optional: true, srf: {type: TextInput}},
+  "vehicleInfo.model": {type: String, optional: true, srf: {type: TextInput}},
+  "vehicleInfo.year": {type: Number, optional: true, srf: {type: NumberInput}},
+  "vehicleInfo.plate": {type: String, optional: true, srf: {type: TextInput}},
+  //owner info
+  "ownerInfo.name": {type: String, optional: true, srf: {type: TextInput}},
+  "ownerInfo.address": {type: String, optional: true, srf: {type: TextInput}},
+  "ownerInfo.phone": {type: String, optional: true, srf: {type: TelInput}},
+  "ownerInfo.email": {type: String, optional: true, srf: {type: EmailInput}},
+  "ownerInfo.license": {type: String, optional: true, srf: {type: TextInput}},
+  //insurance info
+  "insuranceInfo.company": {type: String, optional: true, srf: {type: TextInput}},
+  "insuranceInfo.policyNumber": {type: String, optional: true, srf: {type: TextInput}},
+  "insuranceInfo.agent": {type: String, optional: true, srf: {type: TextInput}},
+  //time & location
+  "timeLocation.date": {type: String, optional: true, srf: {type: DateInput}},
+  "timeLocation.time": {type: String, optional: true, srf: {type: TextInput}},
+  "timeLocation.location": {type: String, optional: true, srf: {type: TextInput}},
+  //Traffic Information
+  "trafficInfo.roadConditions": {type: String, optional: true, srf: {type: TextareaInput}},
+  "trafficInfo.trafficControls": {type: String, optional: true, srf: {type: TextareaInput}},
+  //Witness Information
+  "witnessInfo": {type: Array, optional: true, srf: {type: ArrayField}},
+  "witnessInfo.$": {type: Object, optional: true},
+  "witnessInfo.$.infoPhoto": {type: Array, optional: true, srf: {type: PhotoInput}},
+  "witnessInfo.$.name": {type: String, srf: {type: TextInput}, optional: true},
+  "witnessInfo.$.phone": {type: String, srf: {type: TelInput}, optional: true},
+  "witnessInfo.$.email": {type: String, srf: {type: EmailInput}, optional: true},
+  "witnessInfo.$.testimony": {type: String, srf: {type: TextareaInput}, optional: true},
+  //driver statement
+  "driverStatement": {type: String, srf: {type: TextareaInput}, optional: true},
+  //sketch photo public id
+  "sketch": {type: Array, srf: {type: PhotoInput}, optional: true},
+  //array of photo public ids
+  "photos": {type: Array, srf: {type: PhotoInput}, optional: true},
+  //injuries
+  "injuries": {type: String, srf: {type: PhotoInput}, optional: true},
+  //Symptoms
+  "symptoms": {type: String, optional: true, srf: {type: TextInput}}
 });
 
 Incidents.attachSchema(Schemas.formSchema);
